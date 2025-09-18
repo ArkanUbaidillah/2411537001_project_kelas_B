da98bdf2aab8b9e9c02761ed34632bc7c0691c12
ARKT <arkanwarman@gmail.com>
2025-09-18 12:04:42 +0700

    model2

---
+++ laundryapps/src/model/Pelanggan.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Pelanggan {
+    private int id;
+    private String nama;
+    private String alamat;
+    private String noTelp;
+
+    // Getter & Setter
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public String getAlamat() { return alamat; }
+    public void setAlamat(String alamat) { this.alamat = alamat; }
+
+    public String getNoTelp() { return noTelp; }
+    public void setNoTelp(String noTelp) { this.noTelp = noTelp; }
+	
+	}
+
