4d48e5c3a33bc54e86925a7bae030811b7a73b53
ARKT <arkanwarman@gmail.com>
2025-10-20 15:16:13 +0700

    Praktikum 4

---
+++ laundryapps/src/Praktikum4/TextBook.java
@@ -0,0 +1,12 @@
+package Praktikum4;
+
+public class TextBook extends Book {
+    private String bidangStudi;
+
+    public TextBook(String title, String author, String bidangStudi) {
+        super(title, author);
+        this.bidangStudi = bidangStudi;
+    }
+
+    public String getBidangStudi() { return bidangStudi; }
+}
